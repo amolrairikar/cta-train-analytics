0fb04908a52dfb30a50f5e15cd7e041f3b02c078
= <arairikar1@gmail.com>
2025-05-21 18:29:14 -0500

    Initial CI/CD setup for infrastructure deployment test

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "infra_role_arn" {
+  description = "The ARN for the role assumed by the Terraform user"
+  type        = string
+}
+
+variable "account_number" {
+  description = "The AWS account number"
+  type        = string
+}
+
+variable "email" {
+  description = "Developer email to send notifications to"
+  type        = string
+}
+
+variable "environment" {
+  description = "The deployment environment (QA or PROD)"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The name of the project (to be used in tags)"
+  type        = string
+}
