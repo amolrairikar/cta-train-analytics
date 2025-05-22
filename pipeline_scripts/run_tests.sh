88ca23cc544c9fa0bf59fd00bddc94a28e1f7e6e
= <arairikar1@gmail.com>
2025-05-22 15:30:34 -0500

    Removing pipenv from run_tests.sh

--- pipeline_scripts/run_tests.sh
+++ pipeline_scripts/run_tests.sh
@@ -2,7 +2,7 @@
 
 # Run unit tests
 echo "Running unit tests..."
-if ! pipenv run coverage run --source=src -m unittest discover -s tests/unit; then
+if ! coverage run --source=src -m unittest discover -s tests/unit; then
     echo "Unit tests failed!"
     exit 1
 fi
@@ -10,7 +10,7 @@ mv .coverage .coverage.unit  # Move the coverage data to .coverage.unit
 
 # Run component tests
 echo "Running component tests..."
-if ! pipenv run coverage run --source=src -m unittest discover -s tests/component; then
+if ! coverage run --source=src -m unittest discover -s tests/component; then
     echo "Component tests failed!"
     exit 1
 fi
@@ -18,10 +18,10 @@ mv .coverage .coverage.component  # Move the coverage data to .coverage.componen
 
 # Combine the coverage data
 echo "Combining coverage data..."
-pipenv run coverage combine .coverage.unit .coverage.component
+coverage combine .coverage.unit .coverage.component
 
 # Generate coverage report
 echo "Generating coverage report..."
-pipenv run coverage report --omit="tests/*" --fail-under=80
+coverage report --omit="tests/*" --fail-under=80
 
 echo "Test execution complete!"
