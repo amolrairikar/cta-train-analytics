6458dd7eb0ad3e673816882c338bffe990a04841
= <arairikar1@gmail.com>
2025-05-23 16:12:03 -0500

    Deploying eventbridge scheduler and get gtfs data lambda

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ provider "aws" {
   }
 }
 
-module "spotify_project_data_bucket" {
+# S3 bucket for entire project
+module "cta_train_analytics_project_data_bucket" {
   source         = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/s3-bucket-private?ref=main"
   bucket_prefix  = "cta-train-analytics-data-lake"
   account_number = var.account_number
@@ -18,42 +19,115 @@ module "spotify_project_data_bucket" {
   project        = var.project_name
 }
 
-# data "aws_iam_policy_document" "eventbridge_trust_relationship_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-#     effect  = "Allow"
-#     principals {
-#       type        = "Service"
-#       identifiers = ["scheduler.amazonaws.com"]
-#     }
-#   }
-# }
+# Trust + inline policy documents, role definition, and scheduler definition for EventBridge triggered GTFS fetch Lambda
+data "aws_iam_policy_document" "eventbridge_trust_relationship_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+  }
+}
 
-# data "aws_iam_policy_document" "eventbridge_role_inline_policy_document" {
-#   statement {
-#     effect    = "Allow"
-#     actions   = ["lambda:InvokeFunction"]
-#     resources = [module.cta_fetch_gtfs_lambda.lambda_arn]
-#   }
-# }
+data "aws_iam_policy_document" "eventbridge_role_inline_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["lambda:InvokeFunction"]
+    resources = [module.get_gtfs_data_lambda.lambda_arn]
+  }
+}
 
-# module "eventbridge_role" {
-#   source                    = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/iam-role?ref=main"
-#   role_name                 = "cta-train-analytics-eventbridge-role"
-#   trust_relationship_policy = data.aws_iam_policy_document.eventbridge_trust_relationship_policy.json
-#   inline_policy             = data.aws_iam_policy_document.eventbridge_role_inline_policy_document.json
-#   inline_policy_description = "Policy for EventBridge Scheduler to invoke CTA train analytics project Lambda functions"
-#   environment               = var.environment
-#   project                   = var.project_name
-# }
+module "eventbridge_role" {
+  source                    = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/iam-role?ref=main"
+  role_name                 = "cta-train-analytics-eventbridge-role"
+  trust_relationship_policy = data.aws_iam_policy_document.eventbridge_trust_relationship_policy.json
+  inline_policy             = data.aws_iam_policy_document.eventbridge_role_inline_policy_document.json
+  inline_policy_description = "Policy for EventBridge Scheduler to invoke CTA train analytics project Lambda functions"
+  environment               = var.environment
+  project                   = var.project_name
+}
 
-# module "gtfs_lambda_eventbridge_scheduler" {
-#   source               = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/eventbridge-scheduler?ref=main"
-#   eventbridge_role_arn = module.eventbridge_role.role_arn
-#   lambda_arn           = module.cta_fetch_gtfs_lambda.lambda_arn
-#   schedule_frequency   = "rate(1 week)"
-#   schedule_timezone   = "America/Chicago"
-#   schedule_state       = "ENABLED"
-#   environment          = var.environment
-#   project              = var.project_name
-# }
+module "gtfs_lambda_eventbridge_scheduler" {
+  source               = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/eventbridge-scheduler?ref=main"
+  eventbridge_role_arn = module.eventbridge_role.role_arn
+  lambda_arn           = module.get_gtfs_data_lambda.lambda_arn
+  schedule_frequency   = "cron(1 0 * * 0)"  # Every week on Sunday at 12:01 AM
+  schedule_timezone   = "America/Chicago"
+  schedule_state       = "ENABLED"
+  environment          = var.environment
+  project              = var.project_name
+}
+
+# Generic trust relationship policy for all Lambdas
+data "aws_iam_policy_document" "lambda_trust_relationship_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+# Lambda execution policy, execution role, and function definition for GTFS fetch Lambda
+data "aws_iam_policy_document" "lambda_get_get_gtfs_data_execution_role_inline_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${module.cta_train_analytics_project_data_bucket.bucket_id}"
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions = [
+      "sns:Publish"
+    ]
+    resources = [
+      module.sns_email_subscription.topic_arn
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+module "lambda_get_gtfs_data_played_role" {
+  source                    = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/iam-role?ref=main"
+  role_name                 = "get-gtfs-data-lambda-execution-role"
+  trust_relationship_policy = data.aws_iam_policy_document.lambda_trust_relationship_policy.json
+  inline_policy             = data.aws_iam_policy_document.lambda_get_get_gtfs_data_execution_role_inline_policy_document.json
+  inline_policy_description = "Inline policy for get-gtfs-data Lambda function execution role"
+  environment               = var.environment
+  project                   = var.project_name
+}
+
+module "get_gtfs_data_lambda" {
+  source                         = "git::https://github.com/amolrairikar/aws-account-infrastructure.git//modules/lambda?ref=main"
+  environment                    = var.environment
+  project                        = var.project_name
+  lambda_name                    = "get-gtfs-data-lambda"
+  lambda_description             = "Lambda function to fetch GTFS data from CTA"
+  lambda_filename                = "get_gtfs_data.zip"
+  lambda_handler                 = "main.lambda_handler"
+  lambda_memory_size             = "256"
+  lambda_runtime                 = "python3.12"
+  lambda_execution_role_arn      = module.lambda_get_recently_played_role.role_arn
+  sns_topic_arn                  = module.sns_email_subscription.topic_arn
+    lambda_environment_variables = {
+      S3_BUCKET = module.cta_train_analytics_project_data_bucket.bucket_id
+  }
+}
